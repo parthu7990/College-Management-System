f6eab33aee793ebb050683f47d4d002395e162d8
Parth Prajapati <parth20098@gmail.com>
2026-04-17 10:25:25 +0530

    bad Response Solve

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # 🔐 SECURITY
 SECRET_KEY = os.getenv('SECRET_KEY')
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
