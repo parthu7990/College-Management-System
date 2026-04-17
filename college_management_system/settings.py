baf9156a93cebbf1f1cfe12c6425cc61867c15b5
Parth Prajapati <parth20098@gmail.com>
2026-04-17 10:22:41 +0530

    bad Response Solve

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -11,7 +11,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://*.onrender.com'
+    '.onrender.com'
 ]
 
 CSRF_TRUSTED_ORIGINS = [
