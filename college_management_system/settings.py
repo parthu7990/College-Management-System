911a5a5ad2a1d838f587f64eff04709f50aace4f
Parth Prajapati <parth20098@gmail.com>
2026-04-17 11:09:57 +0530

    updated key

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -78,7 +78,18 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
-
+if os.getenv('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')    
 # LANGUAGE & TIME
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Asia/Kolkata'
